a3369d534ecbfa25e11bf83fae4d5d6a93879c82
Lindomario Santos <lindomario.francisco@gmail.com>
2022-01-26 20:48:05 -0300

    Order, Instant, ISO 8601 - Não estou conseguido criar a tabela TB_USER

--- src/main/java/com/educando/course/conifg/TestConfig.java
+++ src/main/java/com/educando/course/conifg/TestConfig.java
@@ -1,6 +1,5 @@
 package com.educando.course.conifg;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/educando/course/entities/User.java
+++ src/main/java/com/educando/course/entities/User.java
@@ -1,14 +1,19 @@
 package com.educando.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_order")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -20,6 +25,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>(); 
 	
 	public User() {
 		
@@ -74,6 +82,10 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -91,7 +103,4 @@ public class User implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
-
-	
-	
 }
