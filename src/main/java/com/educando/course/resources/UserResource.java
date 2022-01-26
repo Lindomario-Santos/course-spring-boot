750f68f40e580459eefbc32f3a2982e5f27277f3
Lindomario Santos <lindomario.francisco@gmail.com>
2022-01-26 15:26:38 -0300

    user entity and resource

---
+++ src/main/java/com/educando/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.educando.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educando.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Lindomario Francisco dos Santos", "mario@santos.com","98037-2505","123456789");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
